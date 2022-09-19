3d13db21b8ac8174c98dbcb1b3fdc74754af69b9
1wku <beo1692@gmail.com>
2022-09-19 09:29:20 +0700

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -12,4 +12,9 @@ set --universal nvm_default_version v18.4.0
 # pnpm
 set -gx PNPM_HOME "/home/iwku/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
+alias pn=pnpm
 # pnpm end
+
+# Golang
+set -gx GO_HOME "/usr/local/go/bin"
+set -gx PATH "$GO_HOME" $PATH
