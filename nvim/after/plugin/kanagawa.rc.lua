1b8651a4ca541f7948e9ff47a6b376403a798acc
1wku <beo1692@gmail.com>
2022-12-27 14:53:19 +0700

    change theme

--- nvim/after/plugin/kanagawa.rc.lua
+++ nvim/after/plugin/kanagawa.rc.lua
@@ -11,8 +11,8 @@ theme.setup({
     variablebuiltinStyle = { italic = true },
     specialReturn = true, -- special highlight for the return keyword
     specialException = true, -- special highlight for exception handling keywords
-    transparent = true, -- do not set background color
-    dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+    transparent = false, -- do not set background color
+    dimInactive = true, -- dim inactive window `:h hl-NormalNC`
     globalStatus = false, -- adjust window separators highlight for laststatus=3
     terminalColors = true, -- define vim.g.terminal_color_{0,17}
     colors = {
