594306cfd9fbad9d52fa4d5774e238b806b0da9f
ink_tienpn <beo1692@gmail.com.com>
2023-01-06 17:35:51 +0700

    update

--- nvim/after/plugin/prettier.rc.lua
+++ nvim/after/plugin/prettier.rc.lua
@@ -3,7 +3,7 @@ local status, prettier = pcall(require, 'prettier')
 if (not status) then return end
 
 prettier.setup {
-    bin = 'prettier', -- or `'prettierd'` (v0.22+)
+    bin = 'prettierd', -- or `'prettierd'` (v0.22+)
     filetypes = {
         "css",
         "graphql",
