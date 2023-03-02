ed27d5f46d917459f1031a3e45858c503c550aee
Pham Nhat Tien (DC.ODC) <tienpn@inter-k.com>
2023-03-02 09:49:33 +0700

    update nvim

--- nvim/after/plugin/null-ls.rc.lua
+++ nvim/after/plugin/null-ls.rc.lua
@@ -1,39 +1,26 @@
 local status, null_ls = pcall(require, 'null-ls')
 
 if (not status) then return end
-local lSsources = {
-    null_ls.builtins.formatting.prettier.with({
-        filetypes = {
-            "javascript", "typescript", "css", "scss", "html", "json", "yaml", "markdown", "graphql", "md", "txt",
-        },
-    }),
-    null_ls.builtins.diagnostics.eslint_d.with({
-        diagnostics_format = '[eslint] #{m}\n(#{c})',
-    }),
-    null_ls.builtins.diagnostics.fish,
-}
-null_ls.setup({
-    sources = lSsources,
-})
-vim.cmd("autocmd BufWritePost * lua vim.lsp.buf.format()")
 
--- null_ls.setup {
---   on_attach = function(client, bufnr)
---     if client.server_capabilities.documentFormattingProvider then
---       vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.format({async = true})<CR>")
---
---       -- format on save
---       vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.format({})")
---     end
---
---     if client.server_capabilities.documentRangeFormattingProvider then
---       vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
---     end
---   end,
---   sources = {
---     null_ls.builtins.diagnostics.eslint_d.with({
---       diagnostics_format = '[eslint] #{m}\n(#{c})',
---     }),
---     null_ls.builtins.diagnostics.fish,
---   },
--- }
+vim.cmd("autocmd BufWritePost * lua vim.lsp.buf.format({async = true})")
+
+null_ls.setup {
+    on_attach = function(client, bufnr)
+        if client.server_capabilities.documentFormattingProvider then
+            vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.format({async = true})<CR>")
+
+            -- format on save
+            vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.format({})")
+        end
+
+        if client.server_capabilities.documentRangeFormattingProvider then
+            vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+        end
+    end,
+    sources = {
+        null_ls.builtins.diagnostics.eslint_d.with({
+            diagnostics_format = '[eslint] #{m}\n(#{c})',
+        }),
+        null_ls.builtins.diagnostics.fish,
+    },
+}
--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -35,8 +35,8 @@ vim.cmd([[let &t_Ce = "\e[4:0m]"]])
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*",
-  command = "set nopaste",
+    pattern = "*",
+    command = "set nopaste",
 })
 
 -- Add asterisks in block comments
